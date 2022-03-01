b4b970d0fcac36af1e3d46c091262633e0ff1b9a
Nicole Xiang <xinyixia@andrew.cmu.edu>
2022-02-28 19:45:33 -0500

    updated all pages

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # PUI_hw5
 Bonus Features:
 1) Javascript: used to change image on click for the product details page (changing flavors)
-2) Implemented Contact Us and Visit pages
+2) Javascript: clicked images have a border around them (for choosing flavors)
+    Note: you can click once to select a flavor. To select the another flavor, you have to click again ("unclick") on the previous flavor. 
+3) Implemented Contact Us and Visit pages
--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function changeImage(flavor) {
-    // console.log('images/${flavor}Smoothie.png')
+function changeImage(flavor,ele) {
     var source = "images/"+flavor+"Smoothie.png"
     document.getElementById("active_sel").src = source;
+    var flavors = document.getElementsByClassName("f_option")
+    console.log(flavors[ele-1].style.borderWidth == "");
+    if (flavors[ele-1].style.borderWidth == "") {
+        flavors[ele-1].style.border = "thin solid #FFBE0B";
+        console.log(flavors[ele-1].style.border)
+    }
+    else {
+        console.log("unclickin")
+        console.log(flavors[ele-1].style.border)
+        flavors[ele-1].style.border = ""
+    }
+    
 }
